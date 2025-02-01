e79e39b7b314f4edeb60f012a9f9df1d8fe1856e
Lucas Fernando <140983681+OLucasFernando@users.noreply.github.com>
2025-01-31 23:20:36 -0300

    Site On

---
+++ send_email.php
@@ -0,0 +1,25 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST["name"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $message = htmlspecialchars($_POST["message"]);
+
+    $to = "lucasfernando_sousa@outlook.com";  // Substitua pelo seu e-mail
+    $subject = "Nova mensagem de contato";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    $body = "Nome: $name\n";
+    $body .= "E-mail: $email\n";
+    $body .= "Mensagem:\n$message\n";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Mensagem enviada com sucesso!";
+    } else {
+        echo "Erro ao enviar a mensagem.";
+    }
+} else {
+    echo "Método inválido!";
+}
+?>
